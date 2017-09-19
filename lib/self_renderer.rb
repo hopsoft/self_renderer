5d8358d24d584100526fe0f7e468a74d44fda4bd
Nathan Hopkins <natehop@gmail.com>
2017-09-19 16:19:17 -0600

    Minor refactors around extension

--- lib/self_renderer.rb
+++ lib/self_renderer.rb
@@ -5,14 +5,12 @@ require "self_renderer/version"
 module SelfRenderer
   # Renders an HTML string reprsenting this object
   def render_to_html(template: nil, partial: nil, assigns: {}, locals: {})
-    return render_self template: "#{template}.html", assigns: assigns, locals: locals if template.present?
-    render_self partial: "#{partial}.html", assigns: assigns, locals: locals
+    render_self extension: :html, template: template, partial: partial, assigns: assigns, locals: locals
   end
 
   # Renders a JSON string reprsenting this object
   def render_to_json(template: nil, partial: nil, assigns: {}, locals: {})
-    return render_self template: "#{template}.json", assigns: assigns, locals: locals if template.present?
-    render_self partial: "#{partial}.json", assigns: assigns, locals: locals
+    render_self extension: :json, template: template, partial: partial, assigns: assigns, locals: locals
   end
 
   # Returns a Hash representation of this object as rendered by #render_to_json
@@ -26,8 +24,7 @@ module SelfRenderer
       @self_renderer ||= begin
         renderer = ApplicationController.renderer.new
 
-        # HACK: get around limitations in devise/warden when rendering
-        #       views outside the contect of a formal http request
+        # get around limitations in devise/warden when rendering views outside the contect of a formal http request
         if defined? Warden
           env = renderer.instance_eval { @env }
           warden = Warden::Proxy.new(env, Warden::Manager.new(Rails.application))
@@ -38,12 +35,21 @@ module SelfRenderer
       end
     end
 
-    def render_self(template: nil, partial: nil, assigns: {}, locals: {})
-      name = self.class.name.parameterize(separator: "_").to_sym
+    def render_self(extension:, template: nil, partial: nil, assigns: {}, locals: {})
+      name          = self.class.name.parameterize(separator: "_").to_sym
       assigns[name] = self
-      locals[name] = self
-      return self_renderer.render(template, assigns: assigns, locals: locals).squish if template
-      return self_renderer.render(partial: partial, assigns: assigns, locals: locals).squish if partial
+      locals[name]  = self
+
+      if template.present?
+        template = "#{template}.#{extension}" unless template.to_s.end_with?(extension.to_s)
+        return self_renderer.render(template, assigns: assigns, locals: locals).squish
+      end
+
+      if partial.present?
+        partial = "#{partial}.#{extension}" unless partial.to_s.end_with?(extension.to_s)
+        return self_renderer.render(partial: partial, assigns: assigns, locals: locals).squish
+      end
+
       nil
     end
 end
